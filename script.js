8a31a769a97740fba341ce6ef22286f846a62630
JennAnd <jenand0726@skola.goteborg.se>
2023-10-28 20:25:31 +0200

    changes for deployment

--- script.js
+++ script.js
@@ -50,7 +50,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
     // Make a request to the server's endpoint
     fetch(
-      `https://currency-exchange-kappa-swart.vercel.app/convert?amount=${amountTotal}&currency=${currencyTotal}`
+      `https://currency-exchanger-server-d0e77f0a0448.herokuapp.com/convert?amount=${amountTotal}&currency=${currencyTotal}`
     )
       .then((response) => response.json())
       .then((data) => {
